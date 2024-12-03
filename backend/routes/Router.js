23105501e7b2727c47f6ccb8ea50b625cf0e501a
KayoLino <kayofelipelino2002@gmail.com>
2024-12-03 12:55:52 -0300

    Criando Controller do usuário

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const router = express();
+const router = express.Router();
+
+router.use("/api/users", require("./UserRoutes"));
 
 router.get("/", (req, res) => {
   res.send("API Working!")
