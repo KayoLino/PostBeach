7f206800b4d96d8f8c7b134e31b2c85cbabea4c0
KayoLino <kayofelipelino2002@gmail.com>
2024-12-05 10:18:24 -0300

    Resgatando usuário pelo id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -118,11 +118,32 @@ const update = async (req, res) => {
   res.status(200).json(user);
 }
 
+// Get user by id
+const getUserById = async (req, res) => {
+
+  const { id } = req.params;
+  try {
+    const user = await User.findById(new mongoose.Types.ObjectId(id)).select("-password");
+
+    //Check if user exists
+    if (!user) {
+      res.status(404).json({ erros: ["Usuário não encontrado"] });
+    }
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    res.status(404).json({ erros: ["Usuário não encontrado"] });
+  }
+
+}
+
 module.exports = {
   register,
   login,
   getCurrentUser,
-  update
+  update,
+  getUserById,
 }
 
 
