a55dc23ad5b711cfac9daf8502e02787876ec24a
KayoLino <kayofelipelino2002@gmail.com>
2024-12-06 14:38:48 -0300

    Like em foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -115,8 +115,32 @@ const updatePhoto = async (req, res) => {
   }
 
   await photo.save();
-
   res.status(200).json({ photo, message: "Foto atualizada com sucesso." })
+}
+
+// Like functionality
+
+const likePhoto = async (req, res) => {
+  const { id } = req.params;
+  const reqUser = req.user;
+  const photo = await Photo.findById(id);
+
+  // Check if photos exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada."] });
+    return;
+  }
+
+  // Check if user already liked the photo
+  if (photo.likes.includes(reqUser._id)) {
+    res.status(422).json({ errors: ["Você ja curtiu a foto"] });
+    return;
+  }
+
+  // Put user id in likes array
+  photo.likes.push(reqUser._id);
+  photo.save();
+  res.status(200).json({ photoId: id, userId: reqUser._id, message: "A foto foi curtida." })
 }
 
 module.exports = {
@@ -126,4 +150,5 @@ module.exports = {
   getUserPhotos,
   getPhotoById,
   updatePhoto,
+  likePhoto,
 }
