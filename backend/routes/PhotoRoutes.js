476837e1c244e3add99e6fc40cd831a88232e349
KayoLino <kayofelipelino2002@gmail.com>
2024-12-06 10:59:24 -0300

    Excluíndo fotos

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
-const { insertPhoto } = require("../controllers/PhotoController");
+const { insertPhoto, deletePhoto } = require("../controllers/PhotoController");
+
 // Middlewares
 const { photoInsertValidation } = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
@@ -11,5 +12,5 @@ const { imageUpload } = require("../middlewares/imageUpload");
 
 // Routes
 router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
-
+router.delete("/:id", authGuard, deletePhoto);
 module.exports = router;
