4b76c62e4648ac49307a06a5ed073eb3fd4c1485
KayoLino <kayofelipelino2002@gmail.com>
2024-12-16 14:58:28 -0300

    Criando form de login

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,9 +1,34 @@
 import "./Auth.css";
 
+// Components
+import Message from '../../components/Message';
+import { Link } from 'react-router-dom';
+
+// Hooks
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
+
   return (
     <div id="login">
-      Login
+      <h2>PostBeach</h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="E-mail" onChange={e => setEmail(e.target.value)} value={email || ''} />
+        <input type="password" placeholder="Senha" onChange={e => setPassword(e.target.value)} value={password || ''} />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>Não tem uma conta? <Link to="/register">Clique aqui</Link></p>
     </div>
   )
 }
