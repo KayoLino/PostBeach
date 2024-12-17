88a2802f4713894fa7e540c77d40cf2ea19e76aa
KayoLino <kayofelipelino2002@gmail.com>
2024-12-17 18:08:16 -0300

    Slice de atualização de perfil

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -16,7 +16,21 @@ export const profile = createAsyncThunk(
     const data = await userService.profile(user, token);
     return data;
   }
-)
+);
+
+// Update user details
+export const updateProfile = createAsyncThunk(
+  "user/update",
+  async (user, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await userService.updateProfile(user, token);
+
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+    return data
+  }
+);
 
 export const userSlice = createSlice({
   name: "user",
@@ -35,7 +49,22 @@ export const userSlice = createSlice({
       state.success = true;
       state.error = null;
       state.user = action.payload;
-    });
+    })
+    builder.addCase(updateProfile.pending, (state) => {
+      state.loading = true;
+      state.error = false;
+    }).addCase(updateProfile.fulfilled, (state, action) => {
+      state.loading = false;
+      state.success = true;
+      state.error = null;
+      state.user = action.payload;
+      state.message = "Usuário atualizado com sucesso!";
+    }).addCase(updateProfile.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.user = null;
+    })
+      ;
   }
 });
 
