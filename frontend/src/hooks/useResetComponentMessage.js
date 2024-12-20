360a5f1317cea01900b9f8eaceae1356ae0a3faa
KayoLino <kayofelipelino2002@gmail.com>
2024-12-19 23:14:51 -0300

    Implementando mensagem nas fotos e suas funcionalidades

---
+++ frontend/src/hooks/useResetComponentMessage.js
@@ -0,0 +1,10 @@
+// Redux
+import { resetPhotoMessage } from '../slices/photoSlice';
+
+export const useResetComponentMessage = (dispatch) => {
+  return () => {
+    setTimeout(() => {
+      dispatch(resetPhotoMessage());
+    }, 2000);
+  }
+}
--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -6,19 +6,24 @@ import { uploads } from '../../utils/config';
 import Message from "../../components/Message";
 import { Link } from 'react-router-dom';
 import PhotoItem from '../../components/PhotoItem';
+import LikeContainer from '../../components/LikeContainer';
+
 
 // Hooks
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useResetComponentMessage } from '../../hooks/useResetComponentMessage';
 
 // Redux
-import { getPhoto } from '../../slices/photoSlice';
+import { getPhoto, like } from '../../slices/photoSlice';
 
 function Photo() {
 
   const { id } = useParams();
   const dispatch = useDispatch();
+
+  const resetMessage = useResetComponentMessage(dispatch);
 
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector((state) => state.photo);
@@ -30,7 +35,10 @@ function Photo() {
     dispatch(getPhoto(id));
   }, [dispatch, id]);
 
-  // like e comentário
+  const handleLike = () => {
+    dispatch(like(id));
+    resetMessage();
+  }
 
   if (loading) {
     return <p>carregando...</p>
@@ -39,6 +47,12 @@ function Photo() {
   return (
     <div id="photo">
       <PhotoItem photo={photo} />
+      <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+      <div className="message-container">
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+
+      </div>
     </div>
   )
 }
