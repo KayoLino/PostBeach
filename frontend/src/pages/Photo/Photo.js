29670ffdabb47758ece2569687ccedb2e93d19ea
KayoLino <kayofelipelino2002@gmail.com>
2024-12-19 22:16:30 -0300

    Exibindo imagem e dados

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -5,6 +5,7 @@ import { uploads } from '../../utils/config';
 //Components
 import Message from "../../components/Message";
 import { Link } from 'react-router-dom';
+import PhotoItem from '../../components/PhotoItem';
 
 // Hooks
 import { useEffect, useState } from 'react';
@@ -12,11 +13,32 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 // Redux
+import { getPhoto } from '../../slices/photoSlice';
 
 function Photo() {
+
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { photo, loading, error, message } = useSelector((state) => state.photo);
+
+  // comentários
+
+  // Load photo data
+  useEffect(() => {
+    dispatch(getPhoto(id));
+  }, [dispatch, id]);
+
+  // like e comentário
+
+  if (loading) {
+    return <p>carregando...</p>
+  }
+
   return (
-    <div>
-      Photo
+    <div id="photo">
+      <PhotoItem photo={photo} />
     </div>
   )
 }
