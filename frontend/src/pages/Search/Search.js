0d361557b8f95797bce249d37290012e4a320db0
KayoLino <kayofelipelino2002@gmail.com>
2024-12-20 18:26:07 -0300

    Criando página de busca

---
+++ frontend/src/pages/Search/Search.js
@@ -0,0 +1,23 @@
+import './Search.css';
+
+// Hooks
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useResetComponentMessage } from '../../hooks/useResetComponentMessage';
+
+// Components
+import LikeContainer from '../../components/LikeContainer';
+import PhotoItem from '../../components/PhotoItem';
+import { Link } from 'react-router-dom';
+
+//redux 
+
+function Search() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Search
